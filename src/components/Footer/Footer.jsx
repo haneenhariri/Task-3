cbe7d10980a771b886dd149679e2f7775cb1167f
haneen <haneenhariry8@gmail.com>
2024-09-15 14:07:55 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <section className='footer'>
-      <p>Copyright &copy; 2024 Villa Agency Co.,Ltd.All rights reserved. Design:Haneen Al-Hariri</p>
+      <p>Copyright &copy; 2024 Villa Agency Co.,Ltd.All rights reserved. Design: <a href="mailto:haneenhariry@gmail.com">Haneen Al-Hariri</a></p>
     </section>
   )
 }
